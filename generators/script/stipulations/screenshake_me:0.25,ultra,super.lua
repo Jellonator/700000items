064e3b8189db969ef1f3c245891265d06ce30c41
Jellonator <jellonator00@gmail.com>
2017-02-08 00:40:34 -0500

    Removed screen shake for update functions

--- generators/script/stipulations/screenshake_me:0.25,ultra,super.lua
+++ generators/script/stipulations/screenshake_me:0.25,ultra,super.lua
@@ -2,4 +2,8 @@
 python[[
 gen.include("stipulations")
 ]]
-Game():ShakeScreen(5)
+if not time_i then
+    -- to prevent this from working on update frames, I test for the
+    -- time_i local variable which is only present on update functions
+    Game():ShakeScreen(5)
+end
