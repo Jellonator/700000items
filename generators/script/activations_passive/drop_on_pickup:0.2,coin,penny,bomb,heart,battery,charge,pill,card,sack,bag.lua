c6c4fed2bab3ff52a2f93d5306c7c604e59768a7
Jellonator <jellonator00@gmail.com>
2017-02-12 14:02:57 -0500

    Items which drop chests or trinkets when picked up now only drop one

--- generators/script/activations_passive/drop_on_pickup:0.2,coin,penny,bomb,heart,battery,charge,pill,card,sack,bag.lua
+++ generators/script/activations_passive/drop_on_pickup:0.2,coin,penny,bomb,heart,battery,charge,pill,card,sack,bag.lua
@@ -9,8 +9,13 @@ subtype = choose_random_pickup_subtype(pickup)
 pickup_name = get_pickup_name(pickup)
 gen.writeln("{}, {}".format(pickup_name, subtype))
 gen.genstate.add_descriptor(pickup.title())
+gen.set_var("pickup", pickup_name)
         ]]
         local pos = Isaac.GetFreeNearPosition(pos, 1)
         Isaac.Spawn(EntityType.ENTITY_PICKUP, pickup, subtype, pos, Vector(0, 0), nil)
+python[[
+if gen.get_var("pickup") in ["trinket", "chest"]:
+    gen.writeln("break")
+]]
     end
 end
