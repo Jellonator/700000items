18cb429ec6f27e8e394cf73e74f046d67e84241d
James Beedie <jellonator00@gmail.com>
2017-02-14 09:41:34 -0500

    Fixed pickup drop not limiting number of pickups

--- generators/script/activations_passive/drop_on_pickup:0.2,effect-drop.lua
+++ generators/script/activations_passive/drop_on_pickup:0.2,effect-drop.lua
@@ -1,21 +1,21 @@
 python[[gen.genstate.add_descriptors(["Sack", "Squeeze"])]]
 on_pickup = function(self, player)
     local pos = player.Position
-    local num = python[[gen.writeln("{}".format(random.randint(1, 3)))]]
-    for i = 1, num do
-        local pickup, subtype = python[[
+    local pickup, subtype, num = python[[
 pickup = choose_random_pickup(gen.genstate)
 subtype = choose_random_pickup_subtype(pickup)
 pickup_name = get_pickup_name(pickup)
-gen.writeln("{}, {}".format(pickup_name, subtype))
 gen.genstate.add_descriptor(pickup.title())
-gen.set_var("pickup", pickup_name)
-        ]]
+max_num = 3
+if pickup in ["trinket", "chest"]:
+    max_num = 1
+elif pickup in ["pill", "card", "battery", "sack"]:
+    max_num = 2
+num = random.randint(1, max_num)
+gen.writeln("{}, {}, {}".format(pickup_name, subtype, num))
+    ]]
+    for i = 1, num do
         local pos = Isaac.GetFreeNearPosition(pos, 1)
         Isaac.Spawn(EntityType.ENTITY_PICKUP, pickup, subtype, pos, Vector(0, 0), nil)
-python[[
-if gen.get_var("pickup") in ["trinket", "chest"]:
-    gen.writeln("break")
-]]
     end
 end
