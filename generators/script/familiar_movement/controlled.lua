ebb009f03f1a4465c3bb66e01c1426e17cdeebc6
Jellonator <jellonator00@gmail.com>
2017-02-28 18:45:41 -0500

    Made a few extra tweaks

--- generators/script/familiar_movement/controlled.lua
+++ generators/script/familiar_movement/controlled.lua
@@ -1,10 +1,14 @@
 do
     local dir = player:GetAimDirection()
+    local speed = python[[gen.write(random.choice([5, 6, 7, 8]))]]
     if dir.X ~= 0 or dir.Y ~= 0 then
         dir = dir:Normalized()
     end
-    familiar.Velocity = dir * python[[gen.write(random.choice([5, 6, 7, 8]))]]
-    if not Game():GetRoom():IsPositionInRoom(familiar.Position + familiar.Velocity, 1) then
-        familiar.Velocity = Vector(0,0)
+    if not Game():GetRoom():IsPositionInRoom(familiar.Position + Vector(dir.X*(speed+1), 0), 1) then
+        dir.X = 0
     end
+    if not Game():GetRoom():IsPositionInRoom(familiar.Position + Vector(0, dir.Y*(speed+1)), 1) then
+        dir.Y = 0
+    end
+    familiar.Velocity = dir * speed
 end
