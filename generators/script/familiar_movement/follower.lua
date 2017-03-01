39770df78fc5592e3807f23423293af5d7c416eb
Jellonator <jellonator00@gmail.com>
2017-02-28 22:15:16 -0500

    Greatly simplified follower code

--- generators/script/familiar_movement/follower.lua
+++ generators/script/familiar_movement/follower.lua
@@ -1,12 +1,2 @@
-python[[gen.set_var("collision_damage", gen.get_var_default("collision_damage", 0)*3)]]
-local target_pos = player.Position
-local target_distance = python[[gen.write(random.uniform(20, 50))]]
-if (target_pos-pos):LengthSquared() > target_distance^2 then
-    local direction = (target_pos-pos):Normalized()
-    local actual_target = target_pos - direction * target_distance
-    local distance = (actual_target-pos):Length()
-    local speed = distance * 0.25
-    familiar.Velocity = direction * speed * python[[gen.write(random.uniform(0.8, 1.5))]]
-else
-    familiar.Velocity = Vector(0,0)
-end
+familiar.IsFollower = true
+familiar:FollowParent()
